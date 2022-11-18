4f5c404906a1910d0dd37b7acd86476b4671d5af
onestw <117891092+onestw@users.noreply.github.com>
2022-11-18 23:58:03 +0900

    クリックイベントを使ってみた

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
-
+//変数の定義
 const myHeading = document.querySelector('h1');
+//textContentプロパティの値を''に変更する
 myHeading.textContent = 'Hello JavaScript!';
+
+//イベント処理
+let myHTML = document.querySelector('html');
+myHTML.addEventListener('click', function(){
+    alert('ダメ！');
+});
